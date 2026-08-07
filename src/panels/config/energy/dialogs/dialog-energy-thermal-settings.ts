fc9980406bd02e1cc6749406af1f50c3a3915c7a
Youri Westerman <tetracon@gmail.com>
2026-08-07 10:08:01 +0200

    Add thermal settings dialog

---
+++ src/panels/config/energy/dialogs/dialog-energy-thermal-settings.ts
@@ -0,0 +1,385 @@
+import type { CSSResultGroup } from "lit";
+import { css, html, LitElement, nothing } from "lit";
+import { customElement, property, state } from "lit/decorators";
+import { fireEvent } from "../../../../common/dom/fire_event";
+import type { HASSDomCurrentTargetEvent } from "../../../../common/dom/fire_event";
+import "../../../../components/entity/ha-entity-picker";
+import "../../../../components/entity/ha-statistic-picker";
+import "../../../../components/ha-button";
+import "../../../../components/ha-dialog-footer";
+import "../../../../components/ha-markdown";
+import "../../../../components/ha-dialog";
+import "../../../../components/radio/ha-radio-group";
+import type { HaRadioGroup } from "../../../../components/radio/ha-radio-group";
+import "../../../../components/radio/ha-radio-option";
+import "../../../../components/input/ha-input";
+import type { ThermalSourceTypeEnergyPreference } from "../../../../data/energy";
+import {
+  emptyThermalEnergyPreference,
+  energyStatisticHelpUrl,
+} from "../../../../data/energy";
+import {
+  getStatisticLabel,
+  getStatisticMetadata,
+  isExternalStatistic,
+} from "../../../../data/recorder";
+import { getSensorDeviceClassConvertibleUnits } from "../../../../data/sensor";
+import type { HassDialog } from "../../../../dialogs/make-dialog-manager";
+import { DirtyStateProviderMixin } from "../../../../mixins/dirty-state-provider-mixin";
+import { haStyle, haStyleDialog } from "../../../../resources/styles";
+import type { HomeAssistant, ValueChangedEvent } from "../../../../types";
+import type { EnergySettingsThermalDialogParams } from "./show-dialogs-energy";
+import type { HaInput } from "../../../../components/input/ha-input";
+
+type CostType = "no-costs" | "number" | "entity" | "statistic";
+
+interface ThermalFormState {
+  source: ThermalSourceTypeEnergyPreference;
+  costs: CostType;
+}
+
+@customElement("dialog-energy-thermal-settings")
+export class DialogEnergyThermalSettings
+  extends DirtyStateProviderMixin<ThermalFormState>()(LitElement)
+  implements HassDialog<EnergySettingsThermalDialogParams>
+{
+  @property({ attribute: false }) public hass!: HomeAssistant;
+
+  @state() private _params?: EnergySettingsThermalDialogParams;
+
+  @state() private _open = false;
+
+  @state() private _source?: ThermalSourceTypeEnergyPreference;
+
+  @state() private _costs?: CostType;
+
+  @state() private _thermal_units?: string[];
+
+  @state() private _error?: string;
+
+  private _excludeList?: string[];
+
+  private _excludeListFlowRate?: string[];
+
+  public async showDialog(
+    params: EnergySettingsThermalDialogParams
+  ): Promise<void> {
+    this._params = params;
+    this._source = params.source
+      ? { ...params.source }
+      : emptyThermalEnergyPreference();
+    this._costs = this._source.entity_energy_price
+      ? "entity"
+      : this._source.number_energy_price
+        ? "number"
+        : this._source.stat_cost
+          ? "statistic"
+          : "no-costs";
+    this._thermal_units = (
+      await getSensorDeviceClassConvertibleUnits(this.hass, "thermal")
+    ).units;
+    this._excludeList = this._params.thermal_sources
+      .map((entry) => entry.stat_energy_from)
+      .filter((id) => id !== this._source?.stat_energy_from);
+
+    this._open = true;
+    this._initDirtyTracking(
+      { type: "deep" },
+      { source: this._source!, costs: this._costs! }
+    );
+  }
+
+  public closeDialog() {
+    this._open = false;
+    return true;
+  }
+
+  private _dialogClosed() {
+    this._params = undefined;
+    this._source = undefined;
+    this._error = undefined;
+    this._excludeList = undefined;
+    fireEvent(this, "dialog-closed", { dialog: this.localName });
+  }
+
+  protected render() {
+    if (!this._params || !this._source) {
+      return nothing;
+    }
+
+    const pickableUnit = this._thermal_units?.join(", ") || "";
+
+    const unitPriceFixed = `${this.hass.config.currency}/GJ`;
+
+    const externalSource =
+      this._source.stat_energy_from &&
+      isExternalStatistic(this._source.stat_energy_from);
+
+    return html`
+      <ha-dialog
+        .open=${this._open}
+        header-title=${this.hass.localize(
+          "ui.panel.config.energy.thermal.dialog.header"
+        )}
+        .preventScrimClose=${this.isDirtyState}
+        @closed=${this._dialogClosed}
+      >
+        ${this._error ? html`<p class="error">${this._error}</p>` : ""}
+
+        <ha-statistic-picker
+          .hass=${this.hass}
+          .helpMissingEntityUrl=${energyStatisticHelpUrl}
+          include-unit-class="volume"
+          include-device-class="t"
+          .value=${this._source.stat_energy_from}
+          .label=${this.hass.localize(
+            "ui.panel.config.energy.thermal.dialog.thermal_usage"
+          )}
+          .excludeStatistics=${this._excludeList}
+          @value-changed=${this._statisticChanged}
+          .helper=${this.hass.localize(
+            "ui.panel.config.energy.thermal.dialog.entity_para",
+            { unit: pickableUnit }
+          )}
+          autofocus
+        ></ha-statistic-picker>
+
+        <ha-input
+          .label=${this.hass.localize(
+            "ui.panel.config.energy.thermal.dialog.display_name"
+          )}
+          type="text"
+          .disabled=${!this._source?.stat_energy_from}
+          .value=${this._source?.name || ""}
+          .placeholder=${
+            this._source?.stat_energy_from
+              ? getStatisticLabel(
+                  this.hass,
+                  this._source.stat_energy_from,
+                  this._params?.statsMetadata?.[this._source.stat_energy_from]
+                )
+              : ""
+          }
+          @input=${this._nameChanged}
+        >
+        </ha-input>
+
+        <ha-radio-group
+          .label=${this.hass.localize(
+            "ui.panel.config.energy.thermal.dialog.cost_para"
+          )}
+          .value=${this._costs}
+          name="costs"
+          @change=${this._handleCostChanged}
+        >
+          <ha-radio-option value="no-costs">
+            ${this.hass.localize("ui.panel.config.energy.thermal.dialog.no_cost")}
+          </ha-radio-option>
+          <ha-radio-option value="statistic">
+            ${this.hass.localize(
+              "ui.panel.config.energy.thermal.dialog.cost_stat"
+            )}
+          </ha-radio-option>
+          <ha-radio-option value="entity" .disabled=${externalSource}>
+            ${this.hass.localize(
+              "ui.panel.config.energy.thermal.dialog.cost_entity"
+            )}
+          </ha-radio-option>
+          <ha-radio-option value="number" .disabled=${externalSource}>
+            ${this.hass.localize(
+              "ui.panel.config.energy.thermal.dialog.cost_number"
+            )}
+          </ha-radio-option>
+        </ha-radio-group>
+        ${
+          this._costs === "statistic"
+            ? html`<ha-statistic-picker
+                class="price-options"
+                .hass=${this.hass}
+                statistic-types="sum"
+                .value=${this._source.stat_cost}
+                .label=${`${this.hass.localize(
+                  "ui.panel.config.energy.thermal.dialog.cost_stat_input"
+                )} (${this.hass.config.currency})`}
+                @value-changed=${this._priceStatChanged}
+              ></ha-statistic-picker>`
+            : this._costs === "entity"
+              ? html`<ha-entity-picker
+                  class="price-options"
+                  include-domains='["sensor", "input_number"]'
+                  .value=${this._source.entity_energy_price}
+                  .label=${this.hass.localize(
+                    "ui.panel.config.energy.thermal.dialog.cost_entity_input"
+                  )}
+                  .helper=${html`<ha-markdown
+                    .content=${this.hass.localize(
+                      "ui.panel.config.energy.thermal.dialog.cost_entity_helper",
+                      { currency: this.hass.config.currency }
+                    )}
+                  ></ha-markdown>`}
+                  @value-changed=${this._priceEntityChanged}
+                ></ha-entity-picker>`
+              : this._costs === "number"
+                ? html`<ha-input
+                    .label=${`${this.hass.localize(
+                      "ui.panel.config.energy.thermal.dialog.cost_number_input"
+                    )} (${unitPriceFixed})`}
+                    class="price-options"
+                    step="any"
+                    type="number"
+                    .value=${
+                      this._source.number_energy_price !== null
+                        ? String(this._source.number_energy_price)
+                        : ""
+                    }
+                    @change=${this._numberPriceChanged}
+                  >
+                    <span slot="end">${unitPriceFixed}</span>
+                  </ha-input>`
+                : nothing
+        }
+
+        <ha-dialog-footer slot="footer">
+          <ha-button
+            appearance="plain"
+            @click=${this.closeDialog}
+            slot="secondaryAction"
+          >
+            ${this.hass.localize("ui.common.cancel")}
+          </ha-button>
+          <ha-button
+            @click=${this._save}
+            .disabled=${
+              !this._source!.stat_energy_from ||
+              (!!this._params?.source && !this.isDirtyState)
+            }
+            slot="primaryAction"
+          >
+            ${this.hass.localize("ui.common.save")}
+          </ha-button>
+        </ha-dialog-footer>
+      </ha-dialog>
+    `;
+  }
+
+  private _handleCostChanged(ev: HASSDomCurrentTargetEvent<HaRadioGroup>) {
+    this._costs = (ev.currentTarget as HaRadioGroup).value as CostType;
+    this._updateFormDirtyState();
+  }
+
+  private _numberPriceChanged(ev: InputEvent) {
+    this._source = {
+      ...this._source!,
+      number_energy_price: Number((ev.target as HTMLInputElement).value),
+      entity_energy_price: null,
+      stat_cost: null,
+    };
+    this._updateFormDirtyState();
+  }
+
+  private _priceStatChanged(ev: CustomEvent) {
+    this._source = {
+      ...this._source!,
+      entity_energy_price: null,
+      number_energy_price: null,
+      stat_cost: ev.detail.value,
+    };
+    this._updateFormDirtyState();
+  }
+
+  private _priceEntityChanged(ev: CustomEvent) {
+    this._source = {
+      ...this._source!,
+      entity_energy_price: ev.detail.value,
+      number_energy_price: null,
+      stat_cost: null,
+    };
+    this._updateFormDirtyState();
+  }
+
+  private async _statisticChanged(ev: ValueChangedEvent<string>) {
+    if (
+      ev.detail.value &&
+      isExternalStatistic(ev.detail.value) &&
+      this._costs !== "statistic"
+    ) {
+      this._costs = "no-costs";
+    }
+    this._source = {
+      ...this._source!,
+      stat_energy_from: ev.detail.value,
+    };
+
+    if (
+      ev.detail.value &&
+      isExternalStatistic(ev.detail.value) &&
+      this._params?.statsMetadata &&
+      !(ev.detail.value in this._params.statsMetadata)
+    ) {
+      const [metadata] = await getStatisticMetadata(this.hass, [
+        ev.detail.value,
+      ]);
+      if (metadata) {
+        this._params.statsMetadata[ev.detail.value] = metadata;
+        this.requestUpdate("_params");
+      }
+    }
+    this._updateFormDirtyState();
+  }
+
+  private _nameChanged(ev: InputEvent) {
+    this._source = {
+      ...this._source!,
+      name: (ev.target as HaInput).value,
+    };
+    if (!this._source.name) {
+      delete this._source.name;
+    }
+    this._updateFormDirtyState();
+  }
+
+  private _updateFormDirtyState(): void {
+    this._updateDirtyState({ source: this._source!, costs: this._costs! });
+  }
+
+  private async _save() {
+    try {
+      if (this._costs === "no-costs") {
+        this._source!.entity_energy_price = null;
+        this._source!.number_energy_price = null;
+        this._source!.stat_cost = null;
+      }
+      await this._params!.saveCallback(this._source!);
+      this._markDirtyStateClean();
+      this.closeDialog();
+    } catch (err: any) {
+      this._error = err.message;
+    }
+  }
+
+  static get styles(): CSSResultGroup {
+    return [
+      haStyle,
+      haStyleDialog,
+      css`
+        ha-statistic-picker {
+          display: block;
+          margin-bottom: var(--ha-space-4);
+        }
+        ha-radio-group {
+          margin-top: var(--ha-space-4);
+        }
+        .price-options {
+          display: block;
+          margin-top: var(--ha-space-3);
+        }
+      `,
+    ];
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    "dialog-energy-thermal-settings": DialogEnergyThermalSettings;
+  }
+}
